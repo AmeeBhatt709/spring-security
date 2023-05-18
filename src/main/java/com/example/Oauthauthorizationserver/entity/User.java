0371037aa1e8d15cd35c293528f6e9171d602b55
AmeeBhatt709 <ameesbhatt709@gmail.com>
2023-05-18 18:29:22 +0530

    commit

--- src/main/java/com/example/Oauthauthorizationserver/entity/User.java
+++ src/main/java/com/example/Oauthauthorizationserver/entity/User.java
@@ -13,12 +13,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
+
     private String lastName;
+
     private String email;
 
     @Column(length = 60)
     private String password;
 
     private String role;
+
     private boolean enabled = false;
 }
